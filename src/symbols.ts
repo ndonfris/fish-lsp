a7a2c86ae9ed6e0afe80156873d1ee6797524459
Nick Donfris <ndonfris@email.arizona.edu>
2022-10-28 13:45:34 -0500

    removed bad code from 'symbols.ts'

--- src/symbols.ts
+++ src/symbols.ts
@@ -10,10 +10,9 @@ import {SyntaxNode} from 'web-tree-sitter';
 import {logger} from './logger';
 import {execFindDependency} from './utils/exec';
 import {findFunctionScope, isCommand, isCommandFlag, isFunctionDefinintion, scopeCheck, isStatement, isString, isVariable, isVariableDefintion} from './utils/node-types';
-import {ancestorMatch, descendantMatch, getChildNodes, getPrecedingComments, getRange} from './utils/tree-sitter';
+import {getChildNodes, getPrecedingComments, getRange} from './utils/tree-sitter';
 
-
-
+// ~~~~REMOVE IF UNUSED LATER~~~~
 function toSymbolKind(node: SyntaxNode): SymbolKind {
     if (isVariableDefintion(node)) {
         return SymbolKind.Variable
@@ -29,125 +28,6 @@ function toSymbolKind(node: SyntaxNode): SymbolKind {
     return SymbolKind.Null
 }
 
-type lspType = 'definition' | 'refrence';
-
-export interface FishLspSymbol extends SymbolInformation {
-    node: SyntaxNode;
-    lspType: lspType;
-}
-
-
-function toFishLspSymbol(node: SyntaxNode, lspType: lspType, uri: DocumentUri): FishLspSymbol {
-    const symbolType: SymbolKind = toSymbolKind(node);
-    const symbol: FishLspSymbol = {
-        node: node,
-        name: node.text,
-        kind: symbolType,
-        location: {uri: uri, range: getRange(node)},
-        lspType: lspType,
-    }
-    return symbol;
-
-
-}
-
-
-
-//Servers should whenever possible return DocumentSymbol since it is the richer data structure.
-export function getDocumentSymbols(root: SyntaxNode, uri: string) {
-    const symbols: FishLspSymbol[] = [];
-    for (const node of getChildNodes(root)) {
-        if (isFunctionDefinintion(node)) {
-            const symb: FishLspSymbol = toFishLspSymbol(node, 'definition', uri);
-            symbols.push(symb)
-        }
-        if (isVariableDefintion(node)) {
-            const symb = toFishLspSymbol(node, 'definition', uri);
-            symbols.push(symb)
-        }
-        if (isCommand(node)) {
-            const symb = toFishLspSymbol(node, 'refrence', uri);
-            symbols.push(symb)
-        }
-        if (isVariable(node)) {
-            const symb = toFishLspSymbol(node, 'refrence', uri);
-            if (symb.name === "argv") {
-                symb.containerName = ancestorMatch(node, isFunctionDefinintion).at(0)?.child(1)?.text || "";
-            }
-            symbols.push(symb)
-        }
-    }
-    return symbols
-}
-
-
-//export function getDef(uri: string, root: SyntaxNode, node: SyntaxNode,): LocationLink[] {
-//    const def: LocationLink[] = [];
-//    const defintions = getDocumentSymbols(root, uri).filter(sym => sym.lspType === 'definition')
-//
-//    //defintions.includes()
-//    for (const fishSymbol of defintions) {
-//
-//
-//    }
-//    return def;
-//}
-
-//function getVariableSymbols(root: SyntaxNode, map: Map<SyntaxNode, DocumentSymbol[]>): Map<SyntaxNode, DocumentSymbol[]> {
-//    for (const node of getChildNodes(root)) {
-//        const symbolType = toSymbolKind(node);
-//        if (symbolType !== SymbolKind.Variable) {
-//            continue;
-//        }
-//        const symbol: DocumentSymbol = {
-//            name: node.text,
-//            kind: symbolType,
-//            detail: [getPrecedingComments(node), node.text].join('\n'),
-//            selectionRange: getRange(node),
-//            range: getRange(node),
-//        }
-//        map.set(node, [symbol]);
-//    }
-//    return map;
-//}
-
-export function findVariableDefinition(uri: DocumentUri, root: SyntaxNode, node: SyntaxNode): LocationLink[] | undefined {
-    for (const newNode of getChildNodes(root)) {
-        if (isVariableDefintion(newNode) && newNode.text === node.text) {
-            return [{
-                targetUri: uri,
-                targetRange: getRange(newNode),
-                originSelectionRange: getRange(node),
-                targetSelectionRange: getRange(newNode),
-            }]
-        }
-    }
-    return []
-}
-
-//const refrences = ancestorMatch(node,
-//    isVariableDefintion
-//, false).filter((varDef: SyntaxNode) => varDef.text === node.text);
-
-//if (refrences.length === 0) {
-//    return []
-//}
-
-//for (const ref of refrences) {
-//    logger.logNode(ref, 'insideDef')
-//}
-
-//return refrences.map(resultNode => {
-//    return {
-//        originSelectionRange: getRange(node),
-//        targetUri: uri,
-//        targetRange: getRange(resultNode),
-//        targetSelectionRange: getRange(resultNode),
-//    } as LocationLink
-//});
-
-
-
 
 function firstNodeBeforeSecondNodeComaprision(
     firstNode: SyntaxNode,
@@ -156,7 +36,6 @@ function firstNodeBeforeSecondNodeComaprision(
     return (
         firstNode.startPosition.row < secondNode.startPosition.row &&
         firstNode.text == secondNode.text
-        //firstNode.startPosition.column < secondNode.startPosition.column &&
     )
 }
 
@@ -236,6 +115,16 @@ export function getReferences(uri: DocumentUri, root: SyntaxNode, node: SyntaxNo
     return refrences;
 }
 
+/**
+ * finds all LOCAL DocumentSymbols for a document (given by its root). getChildNodes is
+ * used because if we were to instead try searching from the current node upwards, we
+ * have an issue seeing what type of node the current node is. Would be "word", and parent
+ * behavior is therefor non-deterministic. To find a symbol in the document, search
+ * against the results of this function or another function from 'symbols.ts'
+ *
+ * @param {SyntaxNode} root - the root node of the document
+ * @returns {DocumentSymbol[]} - an array of DocumentSymbols
+ */
 export function getLocalSymbols(root: SyntaxNode): DocumentSymbol[] {
     const symbols: DocumentSymbol[] = [];
     for (const node of getChildNodes(root)) {
@@ -263,6 +152,14 @@ export function getLocalSymbols(root: SyntaxNode): DocumentSymbol[] {
     return symbols;
 }
 
+/**
+ * Returns a set of symbols on the given document, where the items in the set are 
+ * only the nearest refrences to leaf param. Used for completionItems.
+ *
+ * @param {SyntaxNode} root - the root node to search from
+ * @param {SyntaxNode} leaf - the node that results should be nearest to
+ * @returns {DocumentSymbol[]} - deduplicated symbols that are nearest to leaf
+ */
 export function getNearestSymbols(root: SyntaxNode, leaf: SyntaxNode): DocumentSymbol[] {
     const symbols: DocumentSymbol[] = getLocalSymbols(root);
     const leafRange = getRange(leaf);
@@ -280,31 +177,17 @@ export function getNearestSymbols(root: SyntaxNode, leaf: SyntaxNode): DocumentS
     return Array.from(filteredSymbols.values());
 }
 
-//export function findLocalCommandDefinitions(node: SyntaxNode, uri: DocumentUri): LocationLink[] | undefined {
-//
-//    const refrences = ancestorMatch(node,
-//        isFunctionDefinintion
-//    , false).filter((command: SyntaxNode) => command.child(1)?.text === node.text);
-//
-//    if (refrences.length === 0) {
-//        return []
-//    }
-//
-//    for (const ref of refrences) {
-//        logger.logNode(ref, 'insideDef')
-//    }
-//
-//    return refrences.map(resultNode => {
-//        return {
-//            originSelectionRange: getRange(node),
-//            targetUri: uri,
-//            targetRange: getRange(resultNode),
-//            targetSelectionRange: getRange(resultNode),
-//        } as LocationLink
-//    });
-//}
 
-
+/**
+ * Returns a locationLink to the function definition matching the name of findNode, in
+ * the root of the document.
+ *
+ * @param {DocumentUri} uri - the uri of the document that 'fish_files/get-dependency.fish' found 
+ * @param {SyntaxNode} root - the root of the new document
+ * @param {SyntaxNode} findNode - the node to find the definition of in the new document
+ * @returns {LocationLink[] | undefined} - the locationLink to the definition of findNode
+ * or empty array if no definition is found
+ */
 export function findGlobalDefinition(uri: DocumentUri, root: SyntaxNode, findNode: SyntaxNode) : LocationLink[] | undefined {
     const results : LocationLink[] = [];
     for (const node of getChildNodes(root)) {
@@ -320,7 +203,4 @@ export function findGlobalDefinition(uri: DocumentUri, root: SyntaxNode, findNod
     }
     return results;
 }
-
-
-
 
