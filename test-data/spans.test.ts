e34c6cfe49d5a7aff53cd46d23f65161c34e08fd
Nick Donfris <ndonfris29@gmail.com>
2023-01-02 19:59:30 -0600

    delete most of this

--- test-data/spans.test.ts
+++ test-data/spans.test.ts
@@ -1,10 +1,12 @@
 import {  getRootNodesFromTexts, logCompareNodes, logDocSymbol, logFile, logNode, logSymbolInfo, logVerboseNode, printDebugSeperator, printTestName } from './helpers';
-import {Point, SyntaxNode} from 'web-tree-sitter';
-import {isScope} from '../src/utils/node-types';
-import {getRange} from '../src/utils/tree-sitter';
+import {Point, SyntaxNode, Tree} from 'web-tree-sitter';
+import {isFunctionDefinitionName, isScope, isVariable, isVariableDefinition} from '../src/utils/node-types';
+import {getRange, nodesGen} from '../src/utils/tree-sitter';
 import { Range, DocumentSymbol } from 'vscode-languageserver'
+import {getChildNodes, positionToPoint} from '../src/utils/tree-sitter';
 import {initializeParser} from '../src/parser';
 import * as colors from 'colors'
+import {toSymbolKind} from '../src/symbols';
 //import
 
 let SHOULD_LOG = true
@@ -17,7 +19,6 @@ beforeEach(() => {
 afterEach(() => {
     global.console = jestConsole;
 });
-
 
 
 interface SpanNode extends SyntaxNode {
@@ -44,16 +45,101 @@ function buildSpans(root: SyntaxNode, spans: SpanNode[] = []): boolean {
     return isSpan
 }
 
+class SpanTree {
+    root: SyntaxNode;
+    childTrees: SpanTree[] = [];
+
+    constructor(root: SyntaxNode) {
+        this.root = root;
+    }
+
+    getChildren(): SpanTree[] {
+        return this.childTrees;
+    }
+
+    getDefinitionChildren(): DocumentSymbol[] {
+        const result : DocumentSymbol[] = []
+        for (const c of nodesGen(this.root)) {
+            if (isFunctionDefinitionName(c)) {
+                const range = getRange(c);
+                const symbol = DocumentSymbol.create(c.text, '', toSymbolKind(c), range, range);
+                result.push(symbol);
+            } else if (isVariableDefinition(c)) {
+                const range = getRange(c);
+                const symbol = DocumentSymbol.create(c.text, '', toSymbolKind(c), range, range);
+                result.push(symbol);
+            }
+        }
+        return result
+    }
+
+    setChildren(children: SpanTree[]) {
+        this.childTrees = children;
+    }
+
+    toString() {
+        const result : string[] = [this.root.type.bgBlack + ', children: ' + this.childTrees.length, ""]
+        for (const c of this.childTrees) {
+            //result.push(getNodeText(c.root, this.root))
+            result.push(c.toString())
+        }
+        return result.join('\n')
+    }
+}
+
+function collectSymbols(root: SyntaxNode, syms: DocumentSymbol[] = []) : boolean {
+    let shouldInclude = isScope(root) || isFunctionDefinitionName(root) || isVariable(root)
+    const children: DocumentSymbol[] = [];
+    for (const c of root.children) {
+        let didAdd = collectSymbols(c, children);
+        shouldInclude =  didAdd || shouldInclude;
+    }
+    if (shouldInclude) {
+        syms.push({
+            name: root.text,
+            kind: toSymbolKind(root),
+            range: getRange(root),
+            selectionRange: getRange(root),
+            children
+        })
+    }
+    return shouldInclude
+}
+
+function buildSpanTree(root: SyntaxNode, spanTree: SpanTree) {
+    let isSpan = isScope(root);
+    const children: SpanTree[] = [];
+    for (const c of root.children) {
+        const childSpan = new SpanTree(c);
+        let didAdd = buildSpanTree(c, childSpan);
+        if (didAdd) {
+            children.push(childSpan);
+        }
+    }
+    if (isSpan) {
+        spanTree.setChildren(children);
+    }
+    return isSpan
+}
+
+
 describe('spans tests', () => {
     it('simple span', async () => {
         const rootNodes = await getRootNodesFromTexts(test_1)
-
         rootNodes.forEach(root => {
             const spans: SpanNode[] = [];
             buildSpans(root, spans);
+            const tree = new SpanTree(root);
+            buildSpanTree(root, tree);
+            const symbols: DocumentSymbol[] = [];
+            collectSymbols(root, symbols)
+            //logSymbol(symbols[0])
+            for (const span of spans) {
+                logSpan(span, root)
+            }
+
             //console.log(spans[0].text?.slice(0, 20));
-            console.log(spans.length)
-            logSpan(spans[0], root)
+            //logSpan(spans[0], root)
         })
 
         expect(true).toBe(true);
@@ -80,56 +166,58 @@ set -l w "world"
 span_test $h $w
 `
 
-function logNodeText(node: SyntaxNode, rootNode: SyntaxNode) {
+function getNodeText(node: SyntaxNode, rootNode: SyntaxNode) {
     const lines = rootNode.text.split('\n')
-    const start = node.startPosition.row
-    const end = node.endPosition.row
+    const start = node.startPosition
+    const end = node.endPosition
     const result: string[] = [];
     lines.forEach((line, row) => {
-        result.push(lineSyntaxHighlighter(line, row, node.startPosition, node.endPosition))
-        //if (row === start || row === end) {
-        //} else if (row > start && row < end) {
-        //    result.push(line.bgBlack)
-        //} else {
-        //    result.push(line)
-        //}
+        let resultLine = '';
+        line.split('').forEach((char, col) => {
+            if (row === start.row && row === end.row) {
+                if (col >= start.column && col <= end.column) {
+                    resultLine += char.bgBlack
+                } else {
+                    resultLine += char
+                }
+            } else if (row === start.row) {
+                if (col >= start.column) {
+                    resultLine += char.bgBlack
+                } else {
+                    resultLine += char
+                }
+            } else if (row === end.row) {
+                if (col < end.column) {
+                    resultLine += char.bgBlack
+                } else {
+                    resultLine += char
+                }
+            } else if (row > start.row && row < end.row) {
+                resultLine += char.bgBlack
+            } else {
+                resultLine += char
+            }
+        })
+        result.push(resultLine)
     })
-    console.log(result.join('\n'))
-
-    //const t = rootNode.text.split('\n').slice(node.startPosition.row)
-    //console.log(`${node.startPosition.row}, ${node.startPosition.column}`);
-    //console.log(`${node.endPosition.row}, ${node.endPosition.column}`);
-    console.log(node.type.black);
-    console.log(node.text.slice(0, 20).black);
-    console.log('-----------------------'.bgBlack)
-
-    //lines.forEach((line, i) => {
-        //// get longest leading whitespace
-        //const leadingWhitespace = line.match(/^\s*/)?.[0] ?? ''
-        //const leadingWhitespaceLength = leadingWhitespace.length
-        //indentAmount = Math.max(indentAmount, leadingWhitespaceLength)
-    //})
-    //console.log(indentAmount)
-    //console.log(t.join('\n').black)
+    return [
+        node.type.bgBlack.blue,
+        result.join('\n').trim(),
+        '-'.repeat(30).bgBlack
+    ].join('\n')
 }
 
-function lineSyntaxHighlighter(line: string, row: number, start: Point, end: Point) {
-    if (row >= start.row && row <= end.row) {
-        //const startCol = start.column;
-        //const endCol = end.column;
-        //const startStr = line.slice(0, startCol);
-        //const endStr = line.slice(endCol);
-        //const midStr = line.slice(startCol, endCol);
-        //return startStr + midStr.bgBlack + endStr;
-        return line.bgBlack
-    } else {
-        return line
+function logSymbol(sym: DocumentSymbol) {
+    console.log(`name: ${sym.name.toString()}`)
+    console.log(`len: ${sym.children?.length.toString()}`)
+    for (const t of sym.children || []) {
+        logSymbol(t)
     }
+    console.log('-'.repeat(30))
 }
-
 function logSpan(n: SpanNode, rootNode: SyntaxNode) {
     //console.log("span: \n".bgRed, n?.text.toString(),)
-    logNodeText(n, rootNode)
+    getNodeText(n, rootNode)
     //console.log(n?.type, n?.startPosition, n?.endPosition);
     n.innerSpans.forEach((child) => {
         logSpan(child, rootNode)
