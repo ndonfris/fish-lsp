d8635f7c63e0c8dbca7669f6d611e08327899207
Nick Donfris <ndonfris29@gmail.com>
2023-01-19 07:11:26 -0600

    removed comments from get-completion.fish

--- fish_files/get-completion.fish
+++ fish_files/get-completion.fish
@@ -2,7 +2,6 @@
 
 function build_cmd --argument-names input
     set --local input_arr (string split --right --max 1 ' ' -- "$input")
-    #set -l last_word (string split --right --max 1 --fields 2 ' ' -- "$input")
     switch "$input_arr[2]"
         case '-*'
             printf "complete --escape --do-complete '$input' | uniq | string match --regex --entire '^\-'"
@@ -17,21 +16,12 @@ end
 
 # from my fish functions
 function get-completions
-    set --local last_word (string split --right --max 1 --fields 2 ' ' -- "$argv")
-    set --local input_str (string escape -n --style=script "$argv")
+    #set --local last_word (string split --right --max 1 --fields 2 ' ' -- "$argv")
+    #set --local input_str (string escape -n --style=script "$argv")
     set --local cmd (build_cmd "$argv")
-    #switch "$last_word"
-    #case '-*'
-    #set cmd complete --escape --do-complete "$input_str" \| uniq \| string match --regex --entire '^\-';
-    ##case ''
-    ##set cmd complete --escape --do-complete "$input_str\ \-" \| uniq \| string match --regex --entire '^\-';
-    #case '**'
-    #set cmd complete --escape --do-complete "$input_str" \| uniq;
-    #end
     eval $cmd
 end
 
 
 
-#test_lastword "$argv"
 get-completions "$argv"
