f84b3efb9beaa1852c1040e233d664e69abde40c
Nick Donfris <ndonfris29@gmail.com>
2023-01-24 00:19:55 -0600

    changed get-completion.fish back to not adding any extra behaviors by default

--- fish_files/get-completion.fish
+++ fish_files/get-completion.fish
@@ -11,16 +11,16 @@
 
 function build_cmd --argument-names input
     set --local input_arr (string split --right --max 1 ' ' -- "$input")
-    switch "$input_arr[2]"
-        case '-*'
-            printf "complete --escape --do-complete '$input' | uniq | string match --regex --entire '^\-'"
-        #case ''
-            #string match -req '^\s?\$' -- "$input_arr[1]";
-            #printf "complete --escape --do-complete '$input' | uniq ";
-            #or printf "complete --escape --do-complete '$input -' | uniq | string match --regex --entire '^\-' && complete --escape --do-complete '$input ' | uniq";
-        case '*'
-            printf "complete --escape --do-complete '$input' | uniq"
-    end
+    #switch "$input_arr[2]"
+        ##case '-*'
+            ##printf "complete --escape --do-complete '$input' | uniq | string match --regex --entire '^\-'"
+        ##case ''
+            ##string match -req '^\s?\$' -- "$input_arr[1]";
+            ##printf "complete --escape --do-complete '$input' | uniq ";
+            ##or printf "complete --escape --do-complete '$input -' | uniq | string match --regex --entire '^\-' && complete --escape --do-complete '$input ' | uniq";
+        #case '*'
+    #end
+    printf "complete --escape --do-complete '$argv' | uniq"
 end
 
 # taken from my fish_config
