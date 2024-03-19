980a2e15b3719ddc9c39e265185bac0fec98060f
ndonfris <ndonfris29@gmail.com>
2024-03-19 07:21:33 -0500

    Release delete

--- scripts/build-fish-wasm.sh
+++ scripts/build-fish-wasm.sh
@@ -2,6 +2,6 @@
 
 yarn add web-tree-sitter
 yarn add --dev tree-sitter-cli https://github.com/ram02z/tree-sitter-fish
-npx tree-sitter build-wasm node_modules/tree-sitter-fish
+npx tree-sitter build -w ./node_modules/tree-sitter-fish/ -o ./tree-sitter-fish.wasm
 
 yarn remove tree-sitter-cli tree-sitter-fish
