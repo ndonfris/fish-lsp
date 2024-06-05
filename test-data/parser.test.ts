d65b5e88c8bbae73a3407561743412281070c056
ndonfris <ndonfris29@gmail.com>
2024-06-05 17:28:54 -0500

    fix: line correction for client `onCommandExecution(connection, line-1)` in src/server.ts

--- test-data/parser.test.ts
+++ test-data/parser.test.ts
@@ -1,24 +1,31 @@
 import Parser from "web-tree-sitter";
 // import {initializeParser} from '../src/parser'
 //import fish from 'tree-sitter-fish'
-import {setLogger} from './helpers'
-import { initializeParser } from '../src/parser'; 
+import { setLogger } from './helpers';
+import { initializeParser } from '../src/parser';
 
-setLogger()
+setLogger();
 
 describe('parser test-suite', () => {
-    it('should be able to load the parser', async () => {
-        // const fish = require('tree-sitter-fish');
-        const parser = await initializeParser()
-        const t = parser.parse('set -gx v "hello world"').rootNode
-        expect(parser).toBeDefined()
-    })
+  it('should be able to load the parser', async () => {
+    // const fish = require('tree-sitter-fish');
+    const parser = await initializeParser();
+    const t = parser.parse('set -gx v "hello world"').rootNode;
+    expect(parser).toBeDefined();
+  });
 
-    it('should parse the fish string', async () => {
-        // const fish = require('tree-sitter-fish');
-        const parser = await initializeParser()
-        const t = parser.parse('set -gx v "hello world"').rootNode
-        expect(parser).toBeDefined()
-        expect(t.children.length).toBeGreaterThanOrEqual(1);
-    })
-})
+  it('should parse the fish string', async () => {
+    // const fish = require('tree-sitter-fish');
+    const parser = await initializeParser();
+    const t = parser.parse('set -gx v "hello world"').rootNode;
+    expect(parser).toBeDefined();
+    expect(t.children.length).toBeGreaterThanOrEqual(1);
+  });
+
+
+  it('matches node types', async () => {
+    const parser = await initializeParser();
+    const {fieldCount} = parser.getLanguage()
+    console.log(fieldCount)
+  });
+});
