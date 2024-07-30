63eeb7b60450d11dfce54bdb76364aa7905e570b
ndonfris <ndonfris29@gmail.com>
2024-07-30 17:36:04 -0500

    feat: minimal `getFishDocumentSymbol(uri, root)` supported symbols.ts

--- test-data/symbols.test.ts
+++ test-data/symbols.test.ts
@@ -2,6 +2,7 @@ import Parser, { SyntaxNode, Range, TreeCursor, Point } from 'web-tree-sitter';
 // import { createMockedSyntaxNode } from './mock-syntax-node';
 import { createFakeLspDocument, setLogger } from './helpers';
 import {
+  // DocumentationBuilder,
   // findAllSymbolItems,
   // SymbolItem,
   // SymbolItemType,
@@ -11,6 +12,7 @@ import {
   // findWorkspaceSymbols,
   // FishDocumentSymbol,
   getFishDocumentSymbolItems,
+  // parseFishFunction,
   // getFishDocumentSymbolItems
 
 } from '../src/utils/symbol';
@@ -18,8 +20,113 @@ import * as TreeSitterUtils from '../src/utils/tree-sitter';
 import * as NodeTypes from '../src/utils/node-types';
 import { initializeParser } from '../src/parser';
 import { md } from '../src/utils/markdown-builder';
+import { getChildNodes } from '../src/utils/tree-sitter';
 // import { DocumentSymbol, WorkspaceSymbol } from 'vscode-languageserver';
 // import { isFunctionDefinition } from '../src/utils/node-types';
+
+import { z } from 'zod';
+import { isOption } from '../src/utils/node-types';
+
+const FishFunctionOptionSchema = z.object({
+  argumentNames: z.array(z.string()).optional(),
+  description: z.string().optional(),
+  wraps: z.string().optional(),
+  onEvent: z.string().optional(),
+  onVariable: z.string().optional(),
+  onJobExit: z.string().optional(),
+  onProcessExit: z.string().optional(),
+  onSignal: z.string().optional(),
+  noScopeShadowing: z.boolean().optional(),
+  inheritVariable: z.string().optional(),
+});
+
+type FishFunctionOption = z.infer<typeof FishFunctionOptionSchema>;
+
+const FishFunctionSchema = z.object({
+  functionName: z.string(),
+  options: FishFunctionOptionSchema,
+});
+
+function collectTillOption(opt: SyntaxNode) {
+  const result: SyntaxNode[] = [];
+  let current: SyntaxNode | null = opt.nextSibling;
+  while (current && !isOption(current)) {
+    if (current && current.type === 'escape_sequence') {
+      continue;
+    }
+    result.push(current);
+    current = current.nextSibling;
+    if (!current || !NodeTypes.isNewline(current)) {
+      return result;
+    }
+  }
+  return result;
+}
+
+type FishFunction = z.infer<typeof FishFunctionSchema>;
+
+function parseFishFunction(node: SyntaxNode): FishFunction {
+  const functionNameNode = node.parent!;
+  const functionName = node.text;
+  console.log({ functionNameNode: functionNameNode.text, children: functionNameNode.childrenForFieldName('option').filter(opt => opt.parent?.equals(functionNameNode)) });
+  const options: FishFunctionOption = {};
+
+  const parameterNodes = functionNameNode.childrenForFieldName('option').filter(opt => opt.parent?.equals(functionNameNode) && isOption(opt));
+  console.log({ params: parameterNodes.map(c => c.text + ':::' + c.type) });
+  for (const paramNode of parameterNodes) {
+    const optionName = paramNode!.text!;
+    const optionValue = collectTillOption(paramNode).map(o => o.text).join(' ');
+    switch (paramNode.type) {
+      case 'word':
+        console.log({ optionName, optionValue, type: paramNode.type });
+        switch (optionName) {
+          case '-a':
+          case '--argument-names':
+            options.argumentNames = optionValue ? optionValue.split(' ') : [];
+            break;
+          case '-d':
+          case '--description':
+            options.description = optionValue;
+            break;
+          case '-w':
+          case '--wraps':
+            options.wraps = optionValue;
+            break;
+          case '-e':
+          case '--on-event':
+            options.onEvent = optionValue;
+            break;
+          case '-v':
+          case '--on-variable':
+            options.onVariable = optionValue;
+            break;
+          case '-j':
+          case '--on-job-exit':
+            options.onJobExit = optionValue;
+            break;
+          case '-p':
+          case '--on-process-exit':
+            options.onProcessExit = optionValue;
+            break;
+          case '-s':
+          case '--on-signal':
+            options.onSignal = optionValue;
+            break;
+          case '-S':
+          case '--no-scope-shadowing':
+            options.noScopeShadowing = true;
+            break;
+          case '-V':
+          case '--inherit-variable':
+            options.inheritVariable = optionValue;
+            break;
+        }
+        break;
+    }
+  }
+
+  return FishFunctionSchema.parse({ functionName, options });
+}
 
 describe('BFS (Breadth First Search) vs DFS (Depth First Search) Iterators', () => {
   // Helper function to create mock SyntaxNodes
@@ -72,7 +179,8 @@ describe('BFS build getDocumentSymbol', () => {
 
   it('test 1: `foo -a a b c; echo $a; echo $b; echo $c; end;`', async () => {
     const doc = createFakeLspDocument('functions/foo.fish', [
-      'function foo -a a b c',
+      'function foo \\',
+      '    -a a b c',
       '    echo $a',
       '    echo $b',
       '    echo $c',
@@ -84,7 +192,17 @@ describe('BFS build getDocumentSymbol', () => {
     ].join('\n'));
     const { rootNode } = parser.parse(doc.getText());
     const symbols = getFishDocumentSymbolItems(doc.uri, rootNode);
-    console.log(symbols.map(s => s.name + '\n' + s.detail + '\n' + md.separator()));
+    const foo = getChildNodes(rootNode).find(n => NodeTypes.isFunctionDefinitionName(n))!.parent!;
+
+    const functionNodes = getChildNodes(rootNode).filter(n => NodeTypes.isFunctionDefinitionName(n));
+
+    functionNodes.forEach(functionNode => {
+      const parsedFunction = parseFishFunction(functionNode);
+      console.log(JSON.stringify(parsedFunction, null, 2));
+    });
+
+    // console.log(parseFishFunction(foo));
+    // console.log(symbols.map(s => s.name + '\n' + s.detail + '\n' + md.separator()));
   });
 
   it('test 2: `function path; path resolve $argv; end;`', async () => {
