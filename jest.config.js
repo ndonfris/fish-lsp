0ec0e2c88454bd1fa55ac1e27a829f139e07377f
Branch Vincent <branchevincent@gmail.com>
2025-01-05 19:53:09 -0800

    ci: run tests and fix flakes

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^tree-sitter$': '<rootDir>/test-data/tree-sitter-jest.js',
+  },
   // transform: { '^.+\.ts?$': ['ts-jest', { isolatedModules: true }] }
   // globals: {
   //   'ts-jest': {
