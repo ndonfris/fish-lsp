0c96c81e74a05a6823ff54258d6ebd9616b7f931
nick <49458459+ndonfris@users.noreply.github.com>
2025-03-04 09:38:15 -0600

    feat: better fishLspEnvVariables support, w/ more info on JSON entries
    - `PrebuiltDocumentationMap` now contains a union of two types
      (`ExtendedBaseJson | EnvVariableJson`)
    - union type is named `ExtendedJson` and has namespaces for `create()`/`is()` on each interface
    - overall not much changes on the `PrebuiltDocumentationMap`, mostly just improved existing
      features
    - `fish-lsp env` now creates output with more info
    - related tests were written in `test-data/snippets.test.ts`

--- src/utils/snippets.ts
+++ src/utils/snippets.ts
@@ -27,7 +27,7 @@ export interface ExtendedBaseJson extends BaseJson {
   // otherTypes: string[]; //TODO
 }
 
-namespace ExtendedBaseJson {
+export namespace ExtendedBaseJson {
   export function create(o: BaseJson, type: JsonType, specialType?: SpecialType): ExtendedBaseJson {
     return {
       ...o,
@@ -36,13 +36,222 @@ namespace ExtendedBaseJson {
       // otherTypes: [type],
     };
   }
+
+  export function is(o: any): o is ExtendedBaseJson {
+    return o.type !== undefined && o.exactMatchOptions === undefined;
+  }
 }
 
-class DocumentationMap {
-  private map: Map<string, ExtendedBaseJson[]> = new Map();
-  private typeMap: Map<JsonType, ExtendedBaseJson[]> = new Map();
+type ValueType = boolean | boolean[] | number | number[] | string | string[];
 
-  constructor(data: ExtendedBaseJson[]) {
+export type CliObject = {
+  name: string;
+  valueType: ValueType;
+  description: string;
+  exactMatchOptions: boolean;
+  type: string;
+  options: string;
+  defaultValue: string;
+};
+
+interface EnvVariableJson extends BaseJson {
+  type: JsonType;
+  specialType: SpecialType;
+  valueType: 'boolean' | 'number' | 'string' | 'array';
+  isDeprecated: boolean;
+  exactMatchOptions: boolean;
+  options: string;
+  defaultValue: string;
+}
+
+export namespace EnvVariableJson {
+  export function create(o: BaseJson | any, exactMatchOptions: boolean, options: ValueType): EnvVariableJson {
+    return {
+      ...o,
+      type: 'variable',
+      specialType: 'fishlsp',
+      isDeprecated: o.isDeprecated || false,
+      exactMatchOptions,
+      options,
+    };
+  }
+
+  export function is(o: any): o is EnvVariableJson {
+    return o.type === 'variable' && o.specialType === 'fishlsp' && o.exactMatchOptions !== undefined;
+  }
+
+  const joinValueTypes = (valueType: ValueType): string => {
+    if (!Array.isArray(valueType)) {
+      return String.raw`${valueType}`;
+    }
+    return valueType.map(v => {
+      if (Number.isInteger(v)) {
+        return v;
+      }
+      return "'" + String.raw`${v}` + "'";
+    }).join(', ');
+  };
+
+  const joinDefaultValue = (valueType: EnvVariableJson['valueType'], defaultValue: ValueType, optionValue: ValueType): string => {
+    if (!Array.isArray(defaultValue)) {
+      if (valueType === 'string' && defaultValue === '') {
+        return `'${defaultValue}'`;
+      } else if (valueType === 'number') {
+        return `${defaultValue}`;
+      } else if (valueType === 'boolean') {
+        return `'${defaultValue}'`;
+      } else if (valueType === 'array') {
+        return '[\'\']';
+      } else {
+        return '';
+      }
+    } else {
+      if (valueType === 'array' && defaultValue.length === 0) {
+        if (Array.isArray(optionValue) && optionValue.some(v => Number.isInteger(v))) {
+          return '[]';
+        }
+        return '[]';
+      } else if (valueType === 'array' && defaultValue.length > 0) {
+        return '[' + joinValueTypes(defaultValue) + ']';
+      }
+      return joinValueTypes(defaultValue);
+    }
+  };
+
+  export function asCliObject(o: EnvVariableJson): CliObject {
+    const options = joinValueTypes(o.options);
+    const defaultValue = joinDefaultValue(o.valueType, o.defaultValue, o.options);
+    return {
+      name: o.name,
+      valueType: o.valueType,
+      description: o.description,
+      exactMatchOptions: o.exactMatchOptions,
+      type: o.type,
+      options,
+      defaultValue,
+    };
+  }
+
+  export function toCliOutput(o: EnvVariableJson, opts: CliToStringOpts = {
+    includeType: true,
+    includeOptions: true,
+    includeDefaultValue: true,
+    wrap: false,
+  }): string {
+    const cli = asCliObject(o);
+    return fromCliOutputToString(cli, opts);
+  }
+
+  export function toMarkdownString(o: EnvVariableJson, opts: CliToStringOpts = {
+    includeType: true,
+    includeOptions: true,
+    includeDefaultValue: true,
+    wrap: true,
+  }): string {
+    const cli = asCliObject(o);
+    return fromCliToMarkdownString(cli, opts);
+  }
+}
+
+function buildBodySection(subtitle: string, body: string, shouldWrap: boolean = false, asMarkdown: boolean = false): string {
+  const hasTitle = () => subtitle.length > 0;
+  const titleStr = !hasTitle() ? '' : `(${subtitle}: `;
+  const trailingBrace = !hasTitle() ? '' : ')';
+  const separator = asMarkdown ? '\n\n' : '\n';
+
+  if (!shouldWrap) return `${titleStr} ${body}${trailingBrace}`;
+
+  const maxLineLength = 76;
+  const output: string[] = [];
+  let currentLine = titleStr;
+  const leftpadBody = !hasTitle() ? '' : ' '.repeat(titleStr.length);
+
+  const splitBody = body.split(' ');
+  const addComma = (idx: number) => idx === splitBody.length - 1 ? '' : ',';
+  const words = asMarkdown
+    ? body.split(' ').map((word, idx) => {
+      const newWord = word !== "''" && !Number.isInteger(word) ? word.slice(0, -1) : word;
+      if (Number.isInteger(newWord)) return md.inlineCode(newWord) + addComma(idx);
+      if (newWord.startsWith("'") && newWord.endsWith("'")) {
+        return md.inlineCode(newWord) + addComma(idx);
+      }
+      return md.inlineCode(word);
+    })
+    : body.split(' ');
+  for (const word of words) {
+    if (currentLine.length + word.length + 1 > maxLineLength) {
+      output.push(currentLine);
+      currentLine = `${leftpadBody}${word} `;
+    } else {
+      currentLine += `${word} `;
+    }
+  }
+  output.push(currentLine);
+  return output.join(separator).trimEnd() + trailingBrace;
+}
+
+type CliToStringOpts = {
+  includeType?: boolean;
+  includeOptions?: boolean;
+  includeDefaultValue?: boolean;
+  wrap?: boolean;
+};
+
+export function fromCliOutputToString(cli: CliObject, opts: CliToStringOpts = {
+  includeType: true,
+  includeOptions: true,
+  includeDefaultValue: true,
+  wrap: false,
+}): string {
+  const title = opts?.includeType ? `$${cli.name} <${cli.valueType.toString().toUpperCase()}>` : cli.name;
+  const body: string[] = [];
+  body.push(...cli.description.split('\n\n'));
+  if (opts.includeOptions) {
+    if (cli.exactMatchOptions) {
+      body.push(buildBodySection('Options', cli.options, opts.wrap));
+    } else {
+      body.push(buildBodySection('Example Options', cli.options, opts.wrap));
+    }
+  }
+  if (opts.includeDefaultValue) body.push(buildBodySection('Default', cli.defaultValue, opts.wrap));
+  return [
+    title,
+    ...body.join('\n').trimEnd().split('\n'),
+  ].map(line => `# ${line}`).join('\n');
+}
+
+export function fromCliToMarkdownString(cli: CliObject, opts: CliToStringOpts = {
+  includeType: true,
+  includeOptions: true,
+  includeDefaultValue: true,
+  wrap: true,
+}): string {
+  const body: string[] = [];
+  if (opts.includeOptions) {
+    if (cli.exactMatchOptions) {
+      body.push(buildBodySection(md.bold('Options'), cli.options, opts.wrap, true));
+    } else {
+      body.push(buildBodySection(md.bold('Example Options'), cli.options, opts.wrap, true));
+    }
+  }
+  if (opts.includeDefaultValue) body.push(buildBodySection(md.bold('Default'), cli.defaultValue, opts.wrap, true));
+  return [
+    `(${md.bold(cli.type)}) ${md.inlineCode(cli.name)} <${cli.valueType.toString().toUpperCase()}>`,
+    cli.description,
+    md.separator(),
+    ...body.join('\n\n').trimEnd().split('\n\n'),
+  ].join('\n\n');
+}
+
+export const fishLspObjs: EnvVariableJson[] = fishlspEnvVariablesJson.map((item: any | BaseJson | Partial<EnvVariableJson>) => EnvVariableJson.create(item, item?.exactMatchOptions, item?.options));
+
+export type ExtendedJson = ExtendedBaseJson | EnvVariableJson;
+
+class DocumentationMap {
+  private map: Map<string, ExtendedJson[]> = new Map();
+  private typeMap: Map<JsonType, ExtendedJson[]> = new Map();
+
+  constructor(data: ExtendedJson[]) {
     data.forEach(item => {
       const curr = this.map.get(item.name) || [];
       // if (this.map.has(item.name)) return
@@ -53,13 +262,13 @@ class DocumentationMap {
     });
   }
 
-  getByName(name: string): ExtendedBaseJson[] {
+  getByName(name: string): ExtendedJson[] {
     return name.startsWith('$')
       ? this.map.get(name.slice(1))?.filter(item => item.type === 'variable') || []
       : this.map.get(name) || [];
   }
 
-  getByType(type: JsonType, specialType?: SpecialType): ExtendedBaseJson[] {
+  getByType(type: JsonType, specialType?: SpecialType): ExtendedJson[] {
     const allOfType = this.typeMap.get(type) || [];
     return specialType !== undefined
       ? allOfType.filter(v => v?.specialType === specialType)
@@ -74,7 +283,7 @@ class DocumentationMap {
     this.typeMap.get(item.type)!.push(item);
   }
 
-  findMatchingNames(query: string, ...types: (JsonType | SpecialType)[]): ExtendedBaseJson[] {
+  findMatchingNames(query: string, ...types: (JsonType | SpecialType)[]): ExtendedJson[] {
     const results: ExtendedBaseJson[] = [];
     this.map.forEach(items => {
       if (items.filter(item => item.name.startsWith(query) && (types.length === 0 || types.includes(item.type || item.specialType)))) {
@@ -105,7 +314,8 @@ const allData: ExtendedBaseJson[] = [
   ...pipeCharactersJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'pipe')),
   ...statusNumbersJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'status')),
   ...themeVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'theme')),
-  ...fishlspEnvVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'fishlsp')),
+  // ...fishlspEnvVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'fishlsp')),
+  ...fishlspEnvVariablesJson.map((item: any | BaseJson | Partial<EnvVariableJson>) => EnvVariableJson.create(item, item?.exactMatchOptions, item?.options)),
   ...envVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'env')),
   ...localeVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'locale')),
   ...specialVariablesJson.map((item: BaseJson) => ExtendedBaseJson.create(item, 'variable', 'special')),
@@ -148,7 +358,7 @@ export function getPrebuiltDocUrl(obj: ExtendedBaseJson): string {
       return `https://fishshell.com/docs/current/language.html#envvar-${obj.name}`;
     case 'special':
       return `https://fishshell.com/docs/current/language.html#envvar-${obj.name}`;
-      // return 'https://fishshell.com/docs/current/language.html#special-variables'
+    // return 'https://fishshell.com/docs/current/language.html#special-variables'
     default:
       return '';
   }
