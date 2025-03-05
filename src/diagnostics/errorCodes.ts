5a6d4bb240eb969939c7c7b7908ec6776224ab3f
nick <49458459+ndonfris@users.noreply.github.com>
2025-03-04 20:31:58 -0600

    feat: added `config.fish_lsp_single_workspace_support` for single workspaces
    - workspace paths are resovled correctly when single workspace support
      is set
    - `analzyer.getDefinition()` gets out of workspace definitions for command names
    - `workspaces` && `currentWorkspace` handle updating the workspaces managed
    - jumping outside of workspaces, currently makes new documents `ReadOnly`
    - `workspaces` now finds completions when inside single workspace mode

--- src/diagnostics/errorCodes.ts
+++ src/diagnostics/errorCodes.ts
@@ -27,6 +27,8 @@ export namespace ErrorCodes {
 
   export const invalidDiagnosticCode = 8001;
 
+  export const syntaxError = 9999;
+
   export type CodeTypes =
     1001 | 1002 | 1003 | 1004 |
     2001 | 2002 | 2003 |
@@ -34,7 +36,8 @@ export namespace ErrorCodes {
     4001 | 4002 | 4003 | 4004 |
     5001 |
     6001 |
-    8001;
+    8001 |
+    9999 ;
 
   export type CodeValueType = {
     severity: DiagnosticSeverity;
@@ -167,6 +170,13 @@ export namespace ErrorCodes {
       codeDescription: { href: 'https://github.com/ndonfris/fish-lsp#environment-variables' },
       source: 'fish-lsp',
       message: 'Deprecated fish-lsp environment variable name',
+    },
+    [syntaxError]: {
+      severity: DiagnosticSeverity.Error,
+      code: syntaxError,
+      codeDescription: { href: 'https://fishshell.com/docs/current/fish_for_bash_users.html#syntax-overview' },
+      source: 'fish-lsp',
+      message: 'fish syntax error',
     },
   };
 
