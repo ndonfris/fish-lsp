b33579a53218af552b9df727c894c62aeec3db2d
nick <49458459+ndonfris@users.noreply.github.com>
2025-03-18 17:32:15 -0500

    feat: added parsing `symbol.details`

--- src/parsing/argparse.ts
+++ src/parsing/argparse.ts
@@ -1,10 +1,11 @@
 import { SyntaxNode } from 'web-tree-sitter';
-import { isOption, isCommandWithName, isEndStdinCharacter, isString, isTopLevelDefinition } from '../utils/node-types';
+import { isCommandWithName, isEndStdinCharacter, isString, isTopLevelDefinition, isEscapeSequence } from '../utils/node-types';
 
-import { Option } from './options';
+import { isMatchingOption, isMatchingOptionOrOptionValue, Option } from './options';
 import { FishSymbol } from './symbol';
 import { LspDocument } from '../document';
 import { DefinitionScope } from '../utils/definition-scope';
+import { getRange } from '../utils/tree-sitter';
 
 export const ArparseOptions = [
   Option.create('-n', '--name').withValue(),
@@ -32,6 +33,65 @@ function getArgparseScopeModifier(document: LspDocument, node: SyntaxNode) {
   }
 }
 
+function createSelectionRange(node: SyntaxNode, flags: string[], flag: string, idx: number) {
+  const range = getRange(node);
+  const text = node.text;
+  if (flags.length === 2 && idx === 0) {
+    if (isString(node)) {
+      range.start = {
+        line: range.start.line,
+        character: range.start.character + 1,
+      };
+      range.end = {
+        line: range.start.line,
+        character: range.start.character - 1,
+      };
+    }
+    return {
+      start: range.start,
+      end: {
+        line: range.start.line,
+        character: range.start.character + flag.length,
+      },
+    };
+  } else if (flags.length === 2 && idx === 1) {
+    return {
+      start: {
+        line: range.start.line,
+        character: range.start.character + text.indexOf('/') + 1,
+      },
+      end: {
+        line: range.end.line,
+        character: range.start.character + text.indexOf('/') + 1 + flag.length,
+      },
+    };
+  } else if (flags.length === 1) {
+    if (isString(node)) {
+      return {
+        start: {
+          line: range.start.line,
+          character: range.start.character + 1,
+        },
+        end: {
+          line: range.end.line,
+          character: range.start.character + 1 + flag.length,
+        },
+      };
+    } else {
+      return getRange(node);
+    }
+  }
+  return range;
+}
+
+export function findArgparseChildren(node: SyntaxNode): SyntaxNode[] {
+  const isBefore = (a: SyntaxNode, b: SyntaxNode) => a.startIndex < b.startIndex;
+  const children = node.childrenForFieldName('argument');
+  const endToken = children.find(n => isEndStdinCharacter(n));
+  if (!endToken) return children;
+  return children.filter(n => isBefore(n, endToken));
+}
+
 export function processArgparseCommand(document: LspDocument, node: SyntaxNode, children: FishSymbol[] = []) {
   const modifier = getArgparseScopeModifier(document, node);
 
@@ -57,7 +117,23 @@ export function processArgparseCommand(document: LspDocument, node: SyntaxNode, 
   // find the parent function or program
   // find all flags before the `--` end token
   const isBefore = (a: SyntaxNode, b: SyntaxNode) => a.startIndex < b.startIndex;
-  const focuesedNodes = node.childrenForFieldName('argument').filter(n => isBefore(n, endChar) && !isOption(n));
+  const focuesedNodes = node.childrenForFieldName('argument')
+    .filter(n => {
+      switch (true) {
+        case isMatchingOptionOrOptionValue(n, Option.create('-X', '--max-args')):
+        case isMatchingOptionOrOptionValue(n, Option.create('-N', '--min-args')):
+        case isMatchingOptionOrOptionValue(n, Option.create('-x', '--exclusive')):
+        case isMatchingOptionOrOptionValue(n, Option.create('-n', '--name')):
+        case isMatchingOption(n, Option.create('-h', '--help')):
+        case isMatchingOption(n, Option.create('-s', '--stop-nonopt')):
+        case isMatchingOption(n, Option.create('-i', '--ignore-unknown')):
+          return false;
+        default:
+          return true;
+      }
+    })
+    .filter(n => !isEscapeSequence(n) && isBefore(n, endChar))
+    ;
 
   const result: FishSymbol[] = [];
   for (const n of focuesedNodes) {
@@ -66,20 +142,26 @@ export function processArgparseCommand(document: LspDocument, node: SyntaxNode, 
     if (isString(n)) {
       flagNames = flagNames.slice(1, -1);
     }
+    if (flagNames.includes('=')) {
+      flagNames = flagNames.slice(0, flagNames.indexOf('='));
+    }
+
     const seenFlags = splitSlash(flagNames);
     const names = getNames(seenFlags);
     // add all seenFlags to the `result: Symb[]` array
-    const flags = names.map(flagName => {
-      return FishSymbol.create(
-        flagName,
-        node,
-        n,
-        'ARGPARSE',
-        document.uri,
-        n.text,
-        DefinitionScope.create(node, modifier),
+    const flags = names.map((flagName, idx) => {
+      const selectedRange = createSelectionRange(node, seenFlags, flagName, idx);
+      return FishSymbol.fromObject({
+        name: flagName,
+        node: node,
+        focusedNode: n,
+        fishKind: 'ARGPARSE',
+        uri: document.uri,
+        detail: n.text,
+        selectionRange: selectedRange,
+        scope: DefinitionScope.create(node.parent!, modifier),
         children,
-      ).addAliasedNames(...names);
+      }).addAliasedNames(...names);
     });
     result.push(...flags);
   }
--- src/parsing/set.ts
+++ src/parsing/set.ts
@@ -1,10 +1,11 @@
 
 import { SyntaxNode } from 'web-tree-sitter';
 import { isOption, isCommandWithName, isTopLevelDefinition } from '../utils/node-types';
-import { Option, findOptionsSet, isMatchingOption } from './options';
+import { Option, findOptions, findOptionsSet, isMatchingOption } from './options';
 import { LspDocument } from '../document';
 import { FishSymbol, SetModifierToScopeTag } from './symbol';
 import { DefinitionScope, ScopeTag } from '../utils/definition-scope';
+import { md } from '../utils/markdown-builder';
 
 export const SetOptions = [
   Option.create('-U', '--universal'),
@@ -26,6 +27,7 @@ export const SetOptions = [
   Option.create('-h', '--help'),
 ];
 
+// const setModifiers = SetOptions.filter(option => option.equalsRawLongOption('--universal', '--global', '--function', '--local'));
 export const SetModifiers = [
   Option.create('-U', '--universal'),
   Option.create('-g', '--global'),
@@ -49,8 +51,39 @@ function getFallbackModifierScope(document: LspDocument, node: SyntaxNode) {
   }
 }
 
+export function findSetChildren(node: SyntaxNode) {
+  const children = node.childrenForFieldName('argument');
+  const firstNonOption = children.findIndex(child => !isOption(child));
+  return children.slice(0, firstNonOption + 1);
+}
+
+export function setModifierDetailDescriptor(nodee: SyntaxNode) {
+  const options = findOptions(nodee.childrenForFieldName('argument'), SetModifiers);
+  const modifier = options.found.find(o => o.option.equalsRawOption('-U', '-g', '-f', '-l'));
+  if (modifier) {
+    switch (true) {
+      case modifier.option.isOption('-U', '--universal'):
+        return `${md.italic('scope')}: ${md.bold('universal')}`;
+      case modifier.option.isOption('-g', '--global'):
+        return `${md.italic('scope')}: ${md.bold('global')}`;
+      case modifier.option.isOption('-f', '--function'):
+        return `${md.italic('scope')}: ${md.bold('function')}`;
+      case modifier.option.isOption('-l', '--local'):
+        return `${md.italic('scope')}: ${md.bold('local')}`;
+      default:
+        return '';
+    }
+  }
+  return '';
+}
+
 export function processSetCommand(document: LspDocument, node: SyntaxNode, children: FishSymbol[] = []) {
-  const definitionNode = node.childrenForFieldName('argument')!.find(n => !isOption(n))!;
+  /** skip `set -q/--query` */
+  if (!isSetDefinition(node)) return [];
+  // create the searchNodes, which are the nodes after the command name, but before the variable name
+  const searchNodes = findSetChildren(node);
+  // find the definition node, which should be the last node of the searchNodes
+  const definitionNode = searchNodes.find(n => !isOption(n))!;
   const modifierOption = findOptionsSet(node.childrenForFieldName('argument'), SetModifiers).pop();
   let modifier = 'local' as ScopeTag;
   if (modifierOption) {
@@ -71,4 +104,3 @@ export function processSetCommand(document: LspDocument, node: SyntaxNode, child
     ),
   ];
 }
-
