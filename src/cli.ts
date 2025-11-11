c625a18c4d4561bdff07dde02b0ec7e3ed94901b
nick <49458459+ndonfris@users.noreply.github.com>
2025-11-10 22:00:27 -0600

    docs(src/cli.ts): remove deprecated `fish-lsp start --help` handlers ('logger', 'index')
    chore: added @jose-elias-alvarez as contributor
    
    Co-Authored-by: @jose-elias-alvarez <jose-elias-alvarez@users.noreply.github.com>

--- src/cli.ts
+++ src/cli.ts
@@ -115,8 +115,8 @@ commandBin.command('start')
     '',
     'Examples:',
     '\t>_ fish-lsp start --disable hover  # only disable the hover feature',
-    '\t>_ fish-lsp start --disable complete logging index hover --dump',
-    '\t>_ fish-lsp start --enable --disable logging complete codeAction',
+    '\t>_ fish-lsp start --disable complete hover --dump',
+    '\t>_ fish-lsp start --enable --disable complete codeAction',
     '\t>_ fish-lsp start --socket 3000  # start TCP server on port 3000 (useful for Docker)',
   ].join('\n'))
   .allowUnknownOption(false)
@@ -284,7 +284,7 @@ commandBin.command('info')
       }
       // handle `[--man-file | --log-file] (--show)?`
       if (args.manFile || args.logFile || args.logsFile) {
-        exitCode = CommanderSubcommand.info.handleFileArgs(args);
+        exitCode = CommanderSubcommand.info.handleFileArgs(args) || 0;
         shouldExit = true;
       }
       // handle `--virtual-fs`
