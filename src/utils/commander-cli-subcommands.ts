ndonfris/fish-lsp

commit 37cb727a79d6f56f192cb63c5e7ea22305934ba1

feat: add cli flag `fish-lsp info --dump-symbol-tree` (#151)
* feat: added cli mode `fish-lsp info --dump-symbol-tree`
   - allowed `fish-lsp info --dump-symbol-tree --no-icons --no-color` to
remove all non ascii characters from output

* docs: update generated files:
  - reformatted man file to align flag descriptions starting at max flag
name text width
  - updated the `src/utils/get-lsp-completionts.ts` file to generate these
completions

* updated the `src/utils/cli-dump-tree.ts` to allow any `--dump-` flag
to correctly take input from stdin or file
  - `fish-lsp info --dump-* -` now recognizes `-` arg as token to denote
input should read from stdin
  - reading from stdin is supported across all 3 `--dump*` operations
`fish-lsp info` provides

src/utils/commander-cli-subcommands.ts
@@ -617,6 +617,8 @@ export namespace CommanderSubcommand {
         status: z.boolean().optional().default(false),
         dumpParseTree: z.union([z.string(), z.boolean()]).optional().default(''),
         dumpSemanticTokens: z.union([z.string(), z.boolean()]).optional().default(''),
+        dumpSymbolTree: z.union([z.string(), z.boolean()]).optional().default(''),
+        icons: z.boolean().optional().default(true),
         virtualFs: z.boolean().optional().default(false),
       }),
     );

man/fish-lsp.1
@@ -17,151 +17,155 @@ Some common language clients include: the builtin API for \fBnvim\fP (v0\.9+), l
 Documentation below shows usage of the \fBfish\-lsp\fP command, including its subcommands and options\.
 .SH OPTIONS
 .P
-\fB\-v\fP or \fB\-\-version\fP           Show version information and exit\.
+\fB\-v\fP or \fB\-\-version\fP                Show version information and exit\.
 .br
-\fB\-h\fP or \fB\-\-help\fP              Show help message and exit\.
+\fB\-h\fP or \fB\-\-help\fP                   Show help message and exit\.
 .br
-\fB\-\-help\-all\fP                Show all the help information
+\fB\-\-help\-all\fP                     Show all the help information
 .br
-\fB\-\-help\-short\fP              Show shortened help message
+\fB\-\-help\-short\fP                   Show shortened help message
 .br
-\fB\-\-help\-man\fP                Show manpage output
+\fB\-\-help\-man\fP                     Show manpage output
 .SH SUBCOMMANDS
 .SS \fBstart\fP
 .P
 Start the language server\.
 .P
-\fB\-\-enable\fP                  enable the language server features
+\fB\-\-enable\fP                       enable the language server features
 .br
-\fB\-\-disable\fP                 disable the language server features
+\fB\-\-disable\fP                      disable the language server features
 .br
-\fB\-\-dump\fP                    dump the json output of the language server features enabled after startup
+\fB\-\-dump\fP                         dump the json output of the language server features enabled after startup
 .br
-\fB\-\-stdio\fP                   use stdin/stdout for communication (default)
+\fB\-\-stdio\fP                        use stdin/stdout for communication (default)
 .br
-\fB\-\-node\-ipc\fP                use node IPC for communication
+\fB\-\-node\-ipc\fP                     use node IPC for communication
 .br
-\fB\-\-socket <port>\fP           use TCP socket for communication
+\fB\-\-socket <port>\fP                use TCP socket for communication
 .br
-\fB\-\-memory\-limit <mb>\fP       set memory usage limit in MB
+\fB\-\-memory\-limit <mb>\fP            set memory usage limit in MB
 .br
-\fB\-\-max\-files <number>\fP      override the maximum number of files to analyze
+\fB\-\-max\-files <number>\fP           override the maximum number of files to analyze
 .SS \fBenv\fP
 .P
 show the environment variables available to the lsp
 .P
-\fB\-c\fP or \fB\-\-create\fP            create the environment variable
+\fB\-c\fP or \fB\-\-create\fP                 create the environment variable
 .br
-\fB\-s\fP or \fB\-\-show\fP              show the environment variables
+\fB\-s\fP or \fB\-\-show\fP                   show the environment variables
 .br
-\fB\-\-show\-default\fP            show the default values for fish\-lsp env variables
+\fB\-\-show\-default\fP                 show the default values for fish\-lsp env variables
 .br
-\fB\-\-only <VAR>\fP              only include the specified environment variables in the output
+\fB\-\-only <VAR>\fP                   only include the specified environment variables in the output
 .br
-\fB\-\-no\-global\fP               don't use global scope when generating environment variables
+\fB\-\-no\-global\fP                    don't use global scope when generating environment variables
 .br
-\fB\-\-no\-local\fP                don't use local scope when generating environment variables
+\fB\-\-no\-local\fP                     don't use local scope when generating environment variables
 .br
-\fB\-\-no\-export\fP               don't use export flag when generating environment variables
+\fB\-\-no\-export\fP                    don't use export flag when generating environment variables
 .br
-\fB\-\-no\-comments\fP             skip outputting comments
+\fB\-\-no\-comments\fP                  skip outputting comments
 .br
-\fB\-\-confd\fP                   output for redirecting to conf\.d/fish\-lsp\.fish
+\fB\-\-confd\fP                        output for redirecting to conf\.d/fish\-lsp\.fish
 .br
-\fB\-\-json\fP                    output \fBfish_lsp_*\fP initialization variables as JSON object (for vscode \fBsettings\.json\fP)
+\fB\-\-json\fP                         output \fBfish_lsp_*\fP initialization variables as JSON object (for vscode \fBsettings\.json\fP)
 .SS \fBinfo\fP
 .P
 show the build info of fish\-lsp
 .P
-\fB\-\-bin\fP                     show the path of the fish\-lsp executable
+\fB\-\-bin\fP                          show the path of the fish\-lsp executable
 .br
-\fB\-\-path\fP                    show the path of the entire fish\-lsp installation
+\fB\-\-path\fP                         show the path of the entire fish\-lsp installation
 .br
-\fB\-\-build\-time\fP              show the path of the entire fish\-lsp repo
+\fB\-\-build\-time\fP                   show the path of the entire fish\-lsp repo
 .br
-\fB\-\-build\-type\fP              show the build type of the command
+\fB\-\-build\-type\fP                   show the build type of the command
 .br
-\fB\-v\fP or \fB\-\-version\fP           show the lsp version
+\fB\-v\fP or \fB\-\-version\fP                show the lsp version
 .br
-\fB\-\-lsp\-version\fP             show the vscode\-languageserver version
+\fB\-\-lsp\-version\fP                  show the vscode\-languageserver version
 .br
-\fB\-\-capabilities\fP            show the lsp capabilities
+\fB\-\-capabilities\fP                 show the lsp capabilities
 .br
-\fB\-\-man\-file\fP                show the man file path
+\fB\-\-man\-file\fP                     show the man file path
 .br
-\fB\-\-log\-file\fP                show the log file path
+\fB\-\-log\-file\fP                     show the log file path
 .br
-\fB\-\-show\fP                    show the man/log file contents (needs to be paired with \fB\-\-log\-file\fP or \fB\-\-man\-file\fP)
+\fB\-\-show\fP                         show the man/log file contents (needs to be paired with \fB\-\-log\-file\fP or \fB\-\-man\-file\fP)
 .br
-\fB\-\-extra\fP                   show debugging server info (capabilities, paths, version, etc\.)
+\fB\-\-extra\fP                        show debugging server info (capabilities, paths, version, etc\.)
 .br
-\fB\-\-verbose\fP                 show debugging server info (capabilities, paths, version, etc\.)
+\fB\-\-verbose\fP                      show debugging server info (capabilities, paths, version, etc\.)
 .br
-\fB\-\-check\-health\fP            run diagnostics and report health status
+\fB\-\-check\-health\fP                 run diagnostics and report health status
 .br
-\fB\-\-health\-check\fP            run diagnostics and report health status
+\fB\-\-health\-check\fP                 run diagnostics and report health status
 .br
-\fB\-\-time\-startup\fP            time the startup of the fish\-lsp executable
+\fB\-\-time\-startup\fP                 time the startup of the fish\-lsp executable
 .br
-\fB\-\-time\-only\fP               show brief summary of the startup timing
+\fB\-\-time\-only\fP                    show brief summary of the startup timing
 .br
-\fB\-\-use\-workspace <PATH>\fP    use the workspace at the specified directory path when \fBfish\-lsp info \-\-time\-startup\fP is used
+\fB\-\-use\-workspace <PATH>\fP         use the workspace at the specified directory path when \fBfish\-lsp info \-\-time\-startup\fP is used
 .br
-\fB\-\-no\-warning\fP              disable message in the \fBfish\-lsp info \-\-time\-startup\fP output
+\fB\-\-no\-warning\fP                   disable message in the \fBfish\-lsp info \-\-time\-startup\fP output
 .br
-\fB\-\-show\-files\fP              show the files that were indexed during startup when \fBfish\-lsp info \-\-time\-startup\fP is used
+\fB\-\-show\-files\fP                   show the files that were indexed during startup when \fBfish\-lsp info \-\-time\-startup\fP is used
 .br
-\fB\-\-dump\-parse\-tree <FILE>\fP  show the tree\-sitter AST for the specified file
+\fB\-\-dump\-symbol\-tree <FILE>\fP      show the fish\-lsp definition symbol tree for the specified file
+.br
+\fB\-\-dump\-parse\-tree <FILE>\fP       show the tree\-sitter AST for the specified file
 .br
 \fB\-\-dump\-semantic\-tokens <FILE>\fP  show the semantic\-tokens for the specified file
 .br
-\fB\-\-no\-color\fP                disable color output in the \fBfish\-lsp info \-\-dump\-file\-tree\fP/\fBfish\-lsp info \-\-semantic\-tokens\fP output
+\fB\-\-no\-color\fP                     disable color output from \fB\-\-dump\-*\fP output
 .br
-\fB\-\-source\-maps\fP             show the source\-maps
+\fB\-\-no\-icons\fP                     disable icon usage in output from \fBfish\-lsp info \-\-dump\-symbol\-tree\fP
 .br
-\fB\-\-check\fP                   used in combination with \fB\-\-source\-maps\fP, verifies source\-maps are working by throwing an error
+\fB\-\-source\-maps\fP                  show the source\-maps
 .br
-\fB\-\-status\fP                  used in combination with \fB\-\-source\-maps\fP, shows status of source\-maps loading
+\fB\-\-check\fP                        used in combination with \fB\-\-source\-maps\fP, verifies source\-maps are working by throwing an error
+.br
+\fB\-\-status\fP                       used in combination with \fB\-\-source\-maps\fP, shows status of source\-maps loading
 .SS \fBurl\fP
 .P
 show a helpful url related to the fish\-lsp
 .P
-\fB\-\-repo\fP or \fB\-\-git\fP           show the github repo
+\fB\-\-repo\fP or \fB\-\-git\fP                show the github repo
 .br
-\fB\-\-npm\fP                     show the npm package url
+\fB\-\-npm\fP                          show the npm package url
 .br
-\fB\-\-homepage\fP                show the homepage
+\fB\-\-homepage\fP                     show the homepage
 .br
-\fB\-\-contributions\fP           show the contributions url
+\fB\-\-contributions\fP                show the contributions url
 .br
-\fB\-\-wiki\fP                    show the github wiki
+\fB\-\-wiki\fP                         show the github wiki
 .br
-\fB\-\-issues\fP or \fB\-\-report\fP      show the issues page
+\fB\-\-issues\fP or \fB\-\-report\fP           show the issues page
 .br
-\fB\-\-discussions\fP             show the discussions page
+\fB\-\-discussions\fP                  show the discussions page
 .br
-\fB\-\-clients\-repo\fP            show the clients configuration repo
+\fB\-\-clients\-repo\fP                 show the clients configuration repo
 .br
-\fB\-\-sources\fP                 show a list of helpful sources
+\fB\-\-sources\fP                      show a list of helpful sources
 .SS \fBcomplete\fP
 .P
 Provide completions for the \fBfish\-lsp\fP
 .P
-\fB\-\-names\fP                   show the feature names of the completions
+\fB\-\-names\fP                        show the feature names of the completions
 .br
-\fB\-\-toggles\fP                 show the feature names of the completions
+\fB\-\-toggles\fP                      show the feature names of the completions
 .br
-\fB\-\-fish\fP                    show fish script
+\fB\-\-fish\fP                         show fish script
 .br
-\fB\-\-features\fP                show features
+\fB\-\-features\fP                     show features
 .br
-\fB\-\-env\-variables\fP           show env variable completions
+\fB\-\-env\-variables\fP                show env variable completions
 .br
-\fB\-\-env\-variable\-names\fP      show env variable names
+\fB\-\-env\-variable\-names\fP           show env variable names
 .br
-\fB\-\-names\-with\-summary\fP      show the names with the summary for the completions
+\fB\-\-names\-with\-summary\fP           show the names with the summary for the completions
 .br
-\fB\-\-abbreviations\fP           show the 'fish\-lsp' subcommand abbreviations
+\fB\-\-abbreviations\fP                show the 'fish\-lsp' subcommand abbreviations
 .SH EXAMPLES
 
 .RS 1
@@ -173,14 +177,14 @@ Start the \fBfish\-lsp\fP language server, with the default configuration:
 .fi
 .RE
 .IP \(bu 2
-Show the path to the \fBfish\-lsp\fP language server binary:
+Generate the completions for the \fBfish\-lsp\fP language server binary:
 .RS 2
 .nf
 >_ fish\-lsp complete > ~/\.config/fish/completions/fish\-lsp\.fish
 .fi
 .RE
 .IP \(bu 2
-Debug the \fBfish\-lsp\fP language server:
+Debug the \fBfish\-lsp\fP language server by dumping the enabled features after startup:
 .RS 2
 .nf
 >_ fish\-lsp start \-\-dump
@@ -219,6 +223,20 @@ Preform a health check on the \fBfish\-lsp\fP language server:
 .RS 2
 .nf
 >_ fish\-lsp info \-\-check\-health
+.fi
+.RE
+.IP \(bu 2
+Show the definition symbol tree for a specific file:
+.RS 2
+.nf
+>_ fish\-lsp info \-\-dump\-symbol\-tree ~/\.config/fish/config\.fish
+.fi
+.RE
+.IP \(bu 2
+Show the semantic tokens for a specific file (read from \fBstdin\fP):
+.RS 2
+.nf
+>_ cat $__fish_data_dir/config\.fish | fish\-lsp info \-\-dump\-semantic\-tokens
 .fi
 .RE
 .IP \(bu 2

src/cli.ts
@@ -7,7 +7,7 @@ import { configHandlers, config, updateHandlers, validHandlers, Config, handleEn
 import { ConnectionOptions, ConnectionType, createConnectionType, maxWidthForOutput, startServer, timeServerStartup } from './utils/startup';
 import { performHealthCheck } from './utils/health-check';
 import { setupProcessEnvExecFile } from './utils/process-env';
-import { handleCLiDumpParseTree, handleCLiDumpSemanticTokens } from './utils/cli-dump-tree';
+import { handleCLiDumpParseTree, handleCLiDumpSemanticTokens, handleCLiDumpSymbolTree } from './utils/cli-dump-tree';
 import PackageJSON from '@package';
 import chalk from 'chalk';
 import vfs from './virtual-fs';
@@ -204,7 +204,9 @@ commandBin.command('info')
   .option('--status', 'show the status of all the source-maps available to the server (use with --source-maps)', false)
   .option('--dump-parse-tree [FILE]', 'dump the tree-sitter parse tree of a file (reads from stdin if no file provided)', undefined)
   .option('--dump-semantic-tokens [FILE]', 'dump the semantic tokens of a file (reads from stdin if no file provided)', undefined)
-  .option('--no-color', 'disable color output for --dump-parse-tree and --dump-semantic-tokens', false)
+  .option('--dump-symbol-tree [FILE]', 'dump the symbol tree of a file (reads from stdin if no file provided)', undefined)
+  .option('--no-color', 'disable color output for --dump-parse-tree, --dump-semantic-tokens, and --dump-symbol-tree', false)
+  .option('--no-icons', 'use plain text tags (f/v/e) instead of nerdfont icons for --dump-symbol-tree')
   .option('--virtual-fs', 'show the virtual filesystem structure (like tree command)', false)
   .allowUnknownOption(false)
   // .allowExcessArguments(false)
@@ -236,6 +238,11 @@ commandBin.command('info')
 
     if (args.dumpSemanticTokens) {
       const status = await handleCLiDumpSemanticTokens(args);
+      process.exit(status);
+    }
+
+    if (args.dumpSymbolTree) {
+      const status = await handleCLiDumpSymbolTree(args);
       process.exit(status);
     }
 

commit 959e982bdcd70dac8b21cd2243fd4217851a79ec

revert: cli behavior for single info flag, `fish-lsp info --bin` -> `/bin/fish-lsp`
* changes from commit 37cb727a79d6f56f192cb63c5e7ea22305934ba1
accidentally did not account for the default value of the new flags
(`--no-color`/`--no-icons`) which caused them to appear as:

```
{ color: true, icons: true }
```

for a basic `fish-lsp info --path` command

causing the output to use pretty print format with title prefix per
line, and prevents using this command for subshell expansions,

e.g., `tail -f (fish-lsp info --log-file)`
has error because unwanted prefix: `Log File: /tmp/fish-lsp.log`

After this commit, our output would be structured:

```fish
>_ fish-lsp info --log-file
/tmp/fish-lsp.log
```

For `fish-lsp info --<flag>` when only one flag is specified.

More than one flag specified, defaults to showing title prefix to denote
what flag corresponds to its value in the output.

src/utils/commander-cli-subcommands.ts
@@ -619,6 +619,7 @@ export namespace CommanderSubcommand {
         dumpSemanticTokens: z.union([z.string(), z.boolean()]).optional().default(''),
         dumpSymbolTree: z.union([z.string(), z.boolean()]).optional().default(''),
         icons: z.boolean().optional().default(true),
+        color: z.boolean().optional().default(true),
         virtualFs: z.boolean().optional().default(false),
       }),
     );
@@ -628,23 +629,25 @@ export namespace CommanderSubcommand {
       return isValidArgs?.success ? isValidArgs.data : schema.parse(args);
     }
     export const defaultSchema: schemaType = schema.parse({});
-    export const skipable = z.object({
-      healhCheck: z.boolean().default(false),
+    export const skippable = z.object({
+      healthCheck: z.boolean().default(false),
       checkHealth: z.boolean().default(false),
       timeStartup: z.boolean().default(false),
       timeOnly: z.boolean().default(false),
       useWorkspace: z.string().default(''),
       warning: z.boolean().default(true),
+      icons: z.boolean().default(true),
+      color: z.boolean().default(true),
     });
-    export type skipableType = z.infer<typeof skipable>;
-    export type skipableArgs = keyof skipableType;
+    export type skippableType = z.infer<typeof skippable>;
+    export type skippableArgs = keyof skippableType;
 
-    export const parseSkip = (args: unknown): z.infer<typeof skipable> => {
-      const isValidArgs = skipable.safeParse(args);
-      return isValidArgs?.success ? isValidArgs.data : skipable.parse(args) || skipable.parse({}); // Validate the args against the schema
+    export const parseSkip = (args: unknown): z.infer<typeof skippable> => {
+      const isValidArgs = skippable.safeParse(args);
+      return isValidArgs?.success ? isValidArgs.data : skippable.parse(args) || skippable.parse({}); // Validate the args against the schema
     };
 
-    export const allSkipableArgvs = [
+    export const allSkippableArgvs = [
       'info',
       '--health-check',
       '--check-health',
@@ -665,8 +668,8 @@ export namespace CommanderSubcommand {
         ].join('\n\n'));
         process.exit(1);
       }
-      const skippedArgs = commandBin.args.filter(arg => arg.startsWith('--') && allSkipableArgvs.some(skipable => arg.startsWith(skipable)));
-      const unrelatedArgs = commandBin.args.filter(arg => arg.startsWith('--') && !allSkipableArgvs.some(skipable => arg.startsWith(skipable)));
+      const skippedArgs = commandBin.args.filter(arg => arg.startsWith('--') && allSkippableArgvs.some(skippable => arg.startsWith(skippable)));
+      const unrelatedArgs = commandBin.args.filter(arg => arg.startsWith('--') && !allSkippableArgvs.some(skippable => arg.startsWith(skippable)));
       if (skippedArgs.length > 0 && unrelatedArgs.length > 0) {
         const unrelatedArgsSeen = argsToString(unrelatedArgs);
         logger.logToStderr([
@@ -943,11 +946,11 @@ export namespace CommanderSubcommand {
     return [...schema.keyof().options];
   };
 
-  export function hasSkipable(command: SubcommandType) {
+  export function hasSkippable(command: SubcommandType) {
     switch (command) {
       case 'info':
         return true;
-      // No skipable
+      // No skippable
       case 'start':
       case 'complete':
       case 'url':
@@ -978,13 +981,15 @@ export namespace CommanderSubcommand {
   export function countArgsWithValues(subcommand: SubcommandType, args: Record<string, unknown>): number {
     const keysToCount = getSubcommand(subcommand).parse(args);
     const results: Record<string, boolean> = {};
-    const skipableArgs = hasSkipable(subcommand);
+    const skippableArgs = hasSkippable(subcommand);
     const removed: Record<string, boolean> = {};
-    if (skipableArgs) {
-      const skipable = info.skipable.parse(args);
-      for (const key in skipable) {
+    if (skippableArgs) {
+      const skippable = info.skippable.parse(args);
+      const defaults = info.skippable.parse({});
+      for (const key in skippable) {
         if (key === subcommand) removed[key] = true;
-        if (skipable[key as keyof typeof skipable]) {
+        removed[key] = true;
+        if (skippable[key as keyof typeof skippable] !== defaults[key as keyof typeof defaults]) {
           removed[key] = false;
         }
       }

src/utils/health-check.ts
@@ -183,13 +183,13 @@ namespace CheckHealthErrorMessages {
       logger.logToStderr('\nTO UPDATE MAN FILE, RUN: ');
       logger.logToStderr([
         '```fish',
-        'fish-lsp info --man-file --show > $MANPATH[1]/fish-lsp.1',
+        'fish-lsp info --man-file --show > $MANPATH[1]/man1/fish-lsp.1',
         '```',
       ].join('\n'));
     },
     globalNotFound: () => {
       logger.logToStdout('✗ global man file not found');
-      logger.logToStderr('\nPLEASE INCLUDE `fish-lsp info --man-file` IN YOUR $MANPATH, or write it to your $MANPATH `fish-lsp info --man-file --show > $MANPATH[1]/man/man1/fish-lsp.1`\n');
+      logger.logToStderr('\nPLEASE INCLUDE `fish-lsp info --man-file` IN YOUR $MANPATH, or write it to your $MANPATH `fish-lsp info --man-file --show > $MANPATH[1]/man1/fish-lsp.1`\n');
     },
   };
 }